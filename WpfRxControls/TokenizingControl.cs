2d21740f0bda08096b9f21c3e89a1bc9aee9d8da
Nigel Magnay <nigel.magnay@gmail.com>
2016-09-16 15:46:11 +0100

    4.6.1 framework targets for x86 and x64.
    Signed-off-by: Nigel Magnay <nigel.magnay@gmail.com>

--- WpfRxControls/TokenizingControl.cs
+++ WpfRxControls/TokenizingControl.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
-using System.Linq;
+
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -19,8 +18,7 @@ namespace WpfRxControls
     {
         public static readonly DependencyProperty TokenTemplateProperty =
             DependencyProperty.Register("TokenTemplate", typeof(DataTemplate), typeof(TokenizingControl));
-
-
+        
         public event TokenChangedDelegate TokenChanged;
 
         public DataTemplate TokenTemplate
